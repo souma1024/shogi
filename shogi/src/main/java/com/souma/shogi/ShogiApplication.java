c3c6798b6359dec29a9758f3c5f929217e790d3b
souma <souma3789522@gmail.com>
2025-06-14 17:52:15 +0900

    first commit

---
+++ shogi/src/main/java/com/souma/shogi/ShogiApplication.java
@@ -0,0 +1,13 @@
+package com.souma.shogi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShogiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShogiApplication.class, args);
+	}
+
+}
